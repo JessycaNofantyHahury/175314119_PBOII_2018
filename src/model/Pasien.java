0c6554c863096b148f6589b1710eadfa3f60bb01
admin <admin@18b>
2018-08-29 12:25:35 +0700

    versi 0.3

--- src/model/Pasien.java
+++ src/model/Pasien.java
@@ -44,6 +44,41 @@ public class Pasien {
     public void setNoRekamMedis(String NoRekamMedis) {
         this.NoRekamMedis = NoRekamMedis;
     }
+
+    public void setTempatLahir(String TempatLahir) {
+        this.TempatLahir = TempatLahir;
+    }
+
+    public void setTanggalLahir(int TanggalLahir)throws Exception {
+        
+        if(TanggalLahir > 0 && TanggalLahir <= 31){
+            this.TanggalLahir = TanggalLahir;
+        }else{
+            throw new Exception("Tanggal Salah...");
+        }
+        
+    }
+
+    public void setBulanLahir(int BulanLahir)throws Exception {
+        
+        if(BulanLahir > 0 && BulanLahir <= 12){
+           this.BulanLahir = BulanLahir; 
+        }else{
+            throw new Exception("Bulan Salah...");
+        }
+        
+    }
+
+    public void setTahunLahir(int TahunLahir)throws Exception {
+        
+        if(TahunLahir > 0){
+        
+        this.TahunLahir = TahunLahir;
+    }else{
+            throw new Exception("Tahun Salah...");
+        }     
+    
+    
     
     
     
@@ -52,4 +87,4 @@ public class Pasien {
     
     
     
-}
+}}
